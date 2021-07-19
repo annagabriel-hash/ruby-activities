6789fd4cdf9bbdb8c855fa264eb52f44ffabb215
Anna Gabriel <yssgabriel@gmail.com>
2021-07-19 19:29:29 +0800

    Tested results

--- vowel_code.rb
+++ vowel_code.rb
@@ -12,3 +12,10 @@ end
 def decode(code, hash_key)
   code.gsub(/[12345]/, hash_key.invert)
 end
+
+# TEST RESULTS
+puts 'Test result'
+25.times {print "-"}
+puts
+puts encode('hello', hash_key) === 'h2ll4'
+puts decode('h3 th2r2', hash_key) === 'hi there'
