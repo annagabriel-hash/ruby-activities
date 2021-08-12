0bf6e99fc1dbdd5b9b1a37436fa3686884b3259a
Anna Gabriel <yssgabriel@gmail.com>
2021-08-12 22:09:21 +0800

    Add test method

--- bouncing_ball.rb
+++ bouncing_ball.rb
@@ -3,3 +3,16 @@ data = {
   {h: 3, bounce: 0.66, window: 1.5} => 3,
   {h: 3, bounce: 1, window: 1.5} => -1,
 }
+def testData(data)
+  # Header
+  puts 'Test result'
+  25.times {print "-"}
+  puts
+  # Results
+  data.each do |key, value|
+    puts bouncing_ball(key[:h], key[:bounce], key[:window]) == value
+  end
+
+end
+
+testData(data)
