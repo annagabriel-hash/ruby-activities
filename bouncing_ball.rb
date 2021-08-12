c6e7ef452ed6511d1804a1a53d1a30ad841f108d
Anna Gabriel <yssgabriel@gmail.com>
2021-08-12 22:09:36 +0800

    Add bouncing ball method

--- bouncing_ball.rb
+++ bouncing_ball.rb
@@ -1,3 +1,14 @@
+def bouncing_ball(h, bounce, window)
+  condition = h > 0 && (bounce > 0 && bounce < 1) && window < h
+  bounce_ball = 1
+  return -1 unless condition
+  while window < h
+    h *= bounce
+    bounce_ball += 1
+  end
+  bounce_ball
+end
+
 # TEST RESULTS
 data = {
   {h: 3, bounce: 0.66, window: 1.5} => 3,
